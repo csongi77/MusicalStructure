b78cff1c145e4570390df63f812a4c55d39a452d
Horváth Csongor <csongor@csbig.cslabs.local>
2018-03-13 12:25:38 +0100

    Finish activity_error.xml

--- app/src/main/java/com/example/csongor/musicalstructure/ErrorActivity.java
+++ app/src/main/java/com/example/csongor/musicalstructure/ErrorActivity.java
@@ -1,13 +1,23 @@
 package com.example.csongor.musicalstructure;
 
+import android.content.Intent;
+import android.icu.text.IDNA;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ErrorActivity extends AppCompatActivity {
 
+    private static final String EXTRA_ERROR = "EXTRA_ERROR";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_goodbye);
+        setContentView(R.layout.activity_error);
+        Intent intent=getIntent();
+        ErrorMessage mErrorMessage=(ErrorMessage)intent.getSerializableExtra(EXTRA_ERROR);
+        switch (mErrorMessage){
+            case NO_MEDIA:
+
+        }
     }
 }
