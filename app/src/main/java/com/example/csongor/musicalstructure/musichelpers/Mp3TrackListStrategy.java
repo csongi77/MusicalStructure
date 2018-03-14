938395068e596a9f1df16c569d304cb77dcac2ca
Horváth Csongor <csongor@csbig.cslabs.local>
2018-03-14 07:51:26 +0100

    fix Mp3TrackListStrategy.java genre parsing

--- app/src/main/java/com/example/csongor/musicalstructure/musichelpers/Mp3TrackListStrategy.java
+++ app/src/main/java/com/example/csongor/musicalstructure/musichelpers/Mp3TrackListStrategy.java
@@ -5,6 +5,8 @@ import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Environment;
 
+import com.example.csongor.musicalstructure.R;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,39 +22,62 @@ class Mp3TrackListStrategy implements PlaylistCreationStrategy {
     private Context mContext;
 
     Mp3TrackListStrategy(Context context) {
-        mContext=context;
+        mContext = context;
     }
 
+    /**
+     * @return new List<Track> containing mp3 files from Music directory and it's subfolders
+     */
     @Override
     public List<Track> getPlaylist() {
-        List<Track> trackList=new ArrayList<>();
+        List<Track> trackList;
         File musicDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
-        File [] fileList = musicDirectory.listFiles();
-        if (fileList.length==0){
+        trackList = processFile(musicDirectory);
+        if (trackList.isEmpty()) {
             trackList.add(new NullTrack());
-        } else {
-            for(int i=0; i<fileList.length; i++){
-                File toProcess=fileList[i];
+        }
+        return trackList;
+    }
+
+    // Parse mp3 files from directory recursively
+    private List<Track> processFile(File file) {
+        File[] fileList = file.listFiles();
+        List<Track> trackList = new ArrayList<>();
+        if (fileList.length == 0)
+            return trackList;
+        for (int i = 0; i < fileList.length; i++) {
+            File toProcess = fileList[i];
+            if (!toProcess.isDirectory()) {
                 String filename = toProcess.getName();
                 String extensionArray[] = filename.split("\\.");
-                if(extensionArray[extensionArray.length-1].equalsIgnoreCase("mp3")){
-                   trackList.add(createMp3Track(toProcess));
+                if (extensionArray[extensionArray.length - 1].equalsIgnoreCase("mp3")) {
+                    // If the file is mp3, new Mp3Track is added to tracklist
+                    trackList.add(createMp3Track(toProcess));
                 }
+            } else {
+                // If the file is directory, it will parsed calling this method recursively
+                trackList.addAll(processFile(fileList[i]));
             }
         }
         return trackList;
     }
 
+    //Creating Mp3Track from file with checking values
     private Track createMp3Track(File toProcess) {
-        MediaMetadataRetriever retriever=new MediaMetadataRetriever();
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
         retriever.setDataSource(toProcess.getPath());
         String artist = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+        if (artist == null || artist.equalsIgnoreCase(""))
+            artist = toProcess.getName().toString();
         String title = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+        if (title == null || title.equalsIgnoreCase(""))
+            title = mContext.getString(R.string.mp3_title_unknown);
         String genre = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_GENRE);
+        if (genre == null || genre.startsWith("(") || genre.equalsIgnoreCase("") || Character.isDigit(genre.charAt(0)))
+            genre = mContext.getString(R.string.mp3_title_unknown);
         long length = Long.parseLong(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
-        Uri uri=Uri.parse(toProcess.getPath());
-        return new Mp3MusicTrack(new MusicTrack(artist,title,length,genre),uri);
+        Uri uri = Uri.parse(toProcess.getPath());
+        return new Mp3MusicTrack(new MusicTrack(artist, title, length, genre), uri);
     }
-
 
 }
